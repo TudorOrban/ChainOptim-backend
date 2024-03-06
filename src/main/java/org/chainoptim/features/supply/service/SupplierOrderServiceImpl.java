eaf3e56805a58f3c95a7f15d796e652177f4620a
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-03-07 00:15:40 +0200

    Add Kafka producer for supplier orders

--- src/main/java/org/chainoptim/features/supply/service/SupplierOrderServiceImpl.java
+++ src/main/java/org/chainoptim/features/supply/service/SupplierOrderServiceImpl.java
@@ -1,5 +1,6 @@
 package org.chainoptim.features.supply.service;
 
+import org.chainoptim.features.supply.dto.CreateSupplierOrderDto;
 import org.chainoptim.features.supply.model.SupplierOrder;
 import org.chainoptim.features.supply.repository.SupplierOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,15 @@ import java.util.List;
 public class SupplierOrderServiceImpl implements SupplierOrderService {
 
     private final SupplierOrderRepository supplierOrderRepository;
+    private final KafkaSupplierOrderService kafkaSupplierOrderService;
 
     @Autowired
-    public SupplierOrderServiceImpl(SupplierOrderRepository supplierOrderRepository) {
+    public SupplierOrderServiceImpl(
+            SupplierOrderRepository supplierOrderRepository,
+            KafkaSupplierOrderService kafkaSupplierOrderService
+    ) {
         this.supplierOrderRepository = supplierOrderRepository;
+        this.kafkaSupplierOrderService = kafkaSupplierOrderService;
     }
 
     public List<SupplierOrder> getSupplierOrdersByOrganizationId(Integer organizationId) {
@@ -23,5 +29,29 @@ public class SupplierOrderServiceImpl implements SupplierOrderService {
 
     public List<SupplierOrder> getSupplierOrdersBySupplierId(Integer supplierId) {
         return supplierOrderRepository.findBySupplierId(supplierId);
+    }
+
+
+    public SupplierOrder saveOrUpdateSupplierOrder(CreateSupplierOrderDto order) {
+        System.out.println("Sending order: " + order.getSupplierId());
+        SupplierOrder supplierOrder = mapCreateDtoToSupplierOrder(order);
+//        SupplierOrder savedOrder = supplierOrderRepository.save(supplierOrder);
+        // Publish order to Kafka broker on create or update
+        kafkaSupplierOrderService.sendSupplierOrder(supplierOrder);
+        return supplierOrder;
+    }
+
+    private SupplierOrder mapCreateDtoToSupplierOrder(CreateSupplierOrderDto order) {
+        SupplierOrder supplierOrder = new SupplierOrder();
+        supplierOrder.setOrganizationId(order.getOrganizationId());
+        supplierOrder.setSupplierId(order.getSupplierId());
+        supplierOrder.setComponentId(order.getComponentId());
+        supplierOrder.setQuantity(order.getQuantity());
+        supplierOrder.setOrderDate(order.getOrderDate());
+        supplierOrder.setEstimatedDeliveryDate(order.getEstimatedDeliveryDate());
+        supplierOrder.setDeliveryDate(order.getDeliveryDate());
+        supplierOrder.setStatus(order.getStatus());
+
+        return supplierOrder;
     }
 }
