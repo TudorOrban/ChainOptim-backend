4169b792c7a640d254d48f99e45ff3ff0feeb774
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-04-18 22:39:41 +0300

    Add pagination for Notifications

--- src/main/java/org/chainoptim/core/notifications/repository/NotificationUserRepository.java
+++ src/main/java/org/chainoptim/core/notifications/repository/NotificationUserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface NotificationUserRepository extends JpaRepository<NotificationUser, Integer> {
+public interface NotificationUserRepository extends JpaRepository<NotificationUser, Integer>, NotificationSearchRepository {
 
     List<NotificationUser> findByUserId(String userId);
 }
