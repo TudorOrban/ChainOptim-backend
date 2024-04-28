e01dcd4066e1387915397c9c6010c5ba7af70abf
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-04-28 11:30:35 +0300

    Add Prometheus and Graphana

--- scripts/Restart.ps1
+++ scripts/Restart.ps1
@@ -9,4 +9,4 @@ Write-Host "Building the Maven project (skipping tests)..."
 mvn clean package -DskipTests
 
 Write-Host "Building and starting Docker containers..."
-docker-compose --profile elk up --build
+docker-compose --profile monitoring up --build
